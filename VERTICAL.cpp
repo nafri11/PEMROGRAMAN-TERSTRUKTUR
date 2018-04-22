5bf3c45b4fffb82cf585359c5196f1d013d7c1c7
putrifebriana <38010113+putrifebriana@users.noreply.github.com>
2018-04-22 07:15:50 -0500

    Update VERTICAL.cpp

--- VERTICAL.cpp
+++ VERTICAL.cpp
@@ -1,37 +1,24 @@
 //fungsi vertical
 
-int a,b;
-for(a=0; a<15; a++)
+for(c=0;c<strlen(words);c++)
 {
-   for(b=0; b<15;b++)
+	if(words[c]==soal[a+c][(b)] )
 	{
-		if(words[0]==soal[b][a] )
-			{
-				for(c=0;c<length;c++)
-				{
-				if(words[c]==soal[b+a][a] )
-				{
-					tmp=c;
-				}
-					else
-					{
-						tmp!=c;
-						break;
-					}
-					cout<<tmp<<endl;
-					
-				}
-			}
-			//if(tmp==(batas-1))
-			//{
-				temp+=1;
-			//}
-			//else
-			//{
-				temp+=0;
-			//}
-		}
+		tmp=c;
+	}
+		else
+	{
+		tmp!=c;
+		break;
+	}
 }
-   
-   
-   
+	if(tmp==(strlen(words)-1))
+	{
+		temp+=1;
+	}
+		else
+	{
+		temp;
+	}
+
+tmp=check[0];
