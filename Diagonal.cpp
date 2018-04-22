b03e37a6e0a6df21ca4089fbfb7871ae2e98a266
rekaamelia014 <38060232+rekaamelia014@users.noreply.github.com>
2018-04-22 11:14:55 +0700

    Add files via upload

---
+++ Diagonal.cpp
@@ -0,0 +1,31 @@
+for(a=0;a<15;a++)
+	{
+		for(b=0;b<15;b++)
+		{
+			if(words[0]==soal[a][b])
+			{
+				for(c=0;c<length;c++)
+				{
+					if(words[c]==soal[a+c][b+c] )
+					{
+						tmp=c;
+			//}
+			//else
+			//{mp=c;
+				    }
+					else
+					{
+						tmp!=c;
+						break;
+					}
+					cout<<tmp<<endl;
+					
+				}
+			}
+			if(tmp==(batas-1))
+			//{
+				temp+=1;
+				temp+=0;
+			//}
+		}
+}
